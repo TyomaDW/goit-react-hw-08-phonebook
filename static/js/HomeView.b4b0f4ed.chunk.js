87534ce5ff404a44d43b1e9647a9f46a7594017c
TyomaDW <81493194+TyomaDW@users.noreply.github.com>
2022-05-05 21:30:14 +0000

    Deploying to gh-pages from @ TyomaDW/goit-react-hw-08-phonebook@48a89ed021adbc01581fec05315ccdb8a3709a1e 🚀

---
+++ static/js/HomeView.b4b0f4ed.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[32],{6561:function(e,t,n){n.r(t);var o=n(890),r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsxs)(o.Z,{sx:{color:"inherit",fontWeight:700},align:"center",mt:32,children:["Welcome to",(0,r.jsx)(o.Z,{sx:{fontWeight:900,color:"#3f51b5"},children:"CONTACTS APP"})]})})}}}]);
+//# sourceMappingURL=HomeView.b4b0f4ed.chunk.js.map
